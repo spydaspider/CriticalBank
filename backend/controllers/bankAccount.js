0deb62ea92896e1ec45ef533724ec1ffbd6cdf7b
spydaspider <spydaspider@gmail.com>
2025-04-03 01:35:11 +0100

    Add RUD operations

--- backend/controllers/bankAccount.js
+++ backend/controllers/bankAccount.js
@@ -1,23 +1,88 @@
 const Account = require('../models/bankAccount.js');
+const mongoose = require('mongoose');
 const generateAccountNumber = require('../helpers/generateAccountNumber.js');
 //create account for holder
-const createAccount = async(req,res)=>{
+const createOrUpdate = async(req,res)=>{
     const user_id = req.user._id;
     const {accountName, idNumber, balance } = req.body;
-    try{
-        //generateAccount Number for the user.
-        const accountNumber = generateAccountNumber();
+    if(!accountName || !idNumber || !balance)
+    {
+        throw Error('Fill in the forms');
+    }
+    const accountExists = await Account.find({user: user_id});
+    if(accountExists){
     
-         const account = await Account.create({accountName,idNumber, balance, user:user_id, accountNumber: 'ACC'+accountNumber });
-        res.status(200).json(account); 
+    
+    try{
+           const account = await Account.findOneAndUpdate({user: user_id},{
+            ...req.body 
+           }, {new:true})
+           res.status(200).json(account);
 
+    }
+    catch(error)
+    {
+         res.status(400).json({error: error.message});
+    }
+    }
+    else{
+        try{
+            //generateAccount Number for the user.
+            const accountNumber = generateAccountNumber();
+        
+             const account = await Account.create({accountName,idNumber, balance, user:user_id, accountNumber: 'ACC'+accountNumber });
+            res.status(200).json(account); 
+    
+        }
+        catch(error)
+        {
+            res.status(400).json({error: error.message});
+        }
+    }
+    
+
+}
+
+const getAccounts = async(req, res)=>{
+    const user_id = req.user._id;
+    try{
+         const account = await Account.find({user: user_id});
+         res.status(200).json(account); 
     }
     catch(error)
     {
         res.status(400).json({error: error.message});
     }
-
+}
+const getAccount = async(req,res)=>{
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(400).json({error: error.message});
+    }
+    try{
+          const account = await Account.findById({_id:id});
+          res.status(200).json(account);
+    }
+    catch(error)
+    {
+         res.status(400).json({error: error.message});
+    }
+}
+const deleteAccount = async(req, res)=>{
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        res.status(400).json({error: error.message});
+    }
+    try{
+          const account = await Account.findOneAndDelete({_id: id});
+          res.status(200).json(account);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
 }
 module.exports = {
-    createAccount
+    createOrUpdate, getAccounts, getAccount, deleteAccount
 }
--- backend/routes/bankAccount.js
+++ backend/routes/bankAccount.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const auth = require('../middleware/auth.js');
-const {createAccount} = require('../controllers/bankAccount.js');
+const {createOrUpdate, getAccounts, getAccount,deleteAccount} = require('../controllers/bankAccount.js');
 const router = express.Router();
 router.use(auth);
-router.post('/', createAccount);
+router.post('/', createOrUpdate);
+router.get('/', getAccounts);
+router.get('/:id', getAccount);
+router.delete('/:id', deleteAccount);
+
 module.exports = router;
