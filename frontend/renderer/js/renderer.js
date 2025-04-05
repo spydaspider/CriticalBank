111fefb5451db2ee4fea887e4980c0414857d3b4
spydaspider <spydaspider@gmail.com>
2025-04-05 14:38:12 +0100

    Add new UI designs

--- frontend/renderer/js/renderer.js
+++ frontend/renderer/js/renderer.js
@@ -8,8 +8,12 @@
     //get the link buttons as well
     const signupNav = document.querySelector(".signup-nav");
     const loginNav = document.querySelector(".login-nav");
+    //get create account elements
+    const createBankAccountPage = document.querySelector(".create-bank-account-page");
+    const createAccountButton = document.getElementById("create-account");
     //Ensure that only the login page shows first until a user click on signup
     loginPage.style.display = "none";
+    createBankAccountPage.style.display = "none";
      //if signup button is clicked from login, show signup page
     toSignupBtn?.addEventListener("click", (e) => {
         e.preventDefault();
@@ -23,15 +27,26 @@
       signupPage.style.display = "none";
       loginPage.style.display = "flex";
     });
+    //if signupNav is clicked
     signupNav?.addEventListener("click", (e)=>{
         e.preventDefault();
         loginPage.style.display = "none";
         signupPage.style.display = "flex";
+        createBankAccountPage.style.display = "none";
     });
+    //if loginNav is clicked
     loginNav?.addEventListener("click", (e)=>{
         e.preventDefault();
         signupPage.style.display = "none";
         loginPage.style.display = "flex";
+        createBankAccountPage.style.display = "none";
+    })
+    //if createAccountButton is clicked
+    createAccountButton?.addEventListener('click',(e)=>{
+           e.preventDefault();
+           signupPage.style.display = "none";
+           loginPage.style.display = "none";
+           createBankAccountPage.style.display = "flex";
     })
 
 
